44019de834db14e91b7a326ae354c9500df207cd
Ishrak <ishrak@example.com>
2026-09-23 00:35:51 +0600

    configure backend for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Server running on http://localhost:${port}`);
   console.log(`Swagger UI at http://localhost:${port}/api`);
 }

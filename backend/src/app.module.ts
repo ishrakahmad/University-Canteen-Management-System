5c5768500c7ec9cde153c1890181a87247503bbc
Ishrak <ishrak@example.com>
2026-09-23 00:45:15 +0600

    configure database port for production

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -29,7 +29,7 @@ import { ReportsModule } from './reports/reports.module';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
+        port: parseInt(configService.get<string>('DB_PORT', '5432'), 10),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
